59ef5068511ad74ad6ed445a3dea8885ee2b8460
Nikita Beletskiy <bel.nikita3197@gmail.com>
2021-02-23 22:19:23 +0300

    Added deployed URL

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const baseUrl = 'http://localhost:4000';
+const baseUrl = 'https://mern-tut-beginners.herokuapp.com';
 
 const App = () => {
   const [name, setName] = useState('');
